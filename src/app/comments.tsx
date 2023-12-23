61eb6f9e9c98e22e259a36ec603cc22b074ba01e
verci <vercilliusjrmila@gmail.com>
2023-12-23 13:17:22 +0800

    comment UI

--- src/app/comments.tsx
+++ src/app/comments.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Image from "next/image";
 
 interface Photo {
   url: string;
@@ -7,6 +8,7 @@ interface Photo {
 interface Comment {
   name: string;
   content: string;
+  photos: Photo[];
 }
 
 const Comments: React.FC = () => {
@@ -18,15 +20,18 @@ const Comments: React.FC = () => {
 
   const fetchComments = async () => {
     const response = await fetch(
-      `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/comments`
+      `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/comments?populate[0]=photos`
     );
     const data = await response.json();
     const reformattedComments = data.data.map((item: any) => ({
-        name: item.attributes.from,
-        content: item.attributes.content,
-      }));
+      name: item.attributes.from,
+      content: item.attributes.content,
+      photos: item.attributes.photos.data.map((photo: any) => ({
+        url: photo.attributes.url,
+      })),
+    }));
 
-      setComments(reformattedComments);
+    setComments(reformattedComments);
   };
 
   return (
@@ -35,6 +40,17 @@ const Comments: React.FC = () => {
         <div key={index} className="bg-white shadow-md rounded-md p-4">
           <h2 className="text-xl font-bold text-black">{comment.name}</h2>
           <p className="text-gray-600">{comment.content}</p>
+          {comment.photos &&
+            comment.photos.map((photo, photoIndex) => (
+              <Image
+                key={index}
+                width={200}
+                height={200}
+                src={`${process.env.NEXT_PUBLIC_STRAPI_URL}${photo.url}`}
+                alt=""
+                style={{ cursor: "pointer" }}
+              />
+            ))}
         </div>
       ))}
     </div>
